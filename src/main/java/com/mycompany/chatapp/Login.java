ec5fa5a63a6f1d7b5fc4c9aece111dc3d4e5031e
Student <Student@LabVM2047644.o5ko0jkowdgurpmknvf2gcsu2h.jnbx.internal.cloudapp.net>
2026-04-12 12:02:32 +0200

    Created Maven project and Validator class Final integrated system with registration, login and chat Running code User commit login running

--- src/main/java/com/mycompany/chatapp/Login.java
+++ src/main/java/com/mycompany/chatapp/Login.java
@@ -2,18 +2,34 @@ package com.mycompany.chatapp;
 
 public class Login {
 
-    // For demo purposes, valid username and password
-    private static final String VALID_USERNAME = "admin";
-    private static final String VALID_PASSWORD = "Abc฿123";
+    private User user;
 
-    // Authenticate method: returns true if username AND password match
-    public boolean authenticate(String username, String password, String username1, String password1) {
-        // Check for null inputs to avoid errors
-        if (username == null || password == null) {
+    // Register user
+    public void register(User user) {
+        this.user = user;
+    }
+
+    // Check login details
+    
+
+    public boolean loginUser(String username, String password) {
+        
+        if (user == null) {
+            System.out.println("No registered user found.");
             return false;
         }
-
-        // Compare input with valid credentials
-        return username.equals(VALID_USERNAME) && password.equals(VALID_PASSWORD);
+        Object usename = null;
+        
+        return user.username.equals(usename) &&
+                user.password.equals(password);
+    }
+    // Return message
+    public String returnLoginStatus(boolean status) {
+        if (status) {
+            return "Welcome " + user.firstName + ", " + user.lastName + " it is great to see you again.";
+        } else {
+            return "Username or password incorrect, please try again.";
+        }
     }
 }
+
